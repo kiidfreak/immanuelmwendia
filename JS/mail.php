59ec16736390e3c4a0ba1164d44ef91e6beb4116
Immanuel Maina <imaina671@gmail.com>
2021-09-18 08:03:16 +0300

    missing semicolon added on mail.php

--- JS/mail.php
+++ JS/mail.php
@@ -1,7 +1,8 @@
 <?php
 if($_POST["message"];
    $_POST["name"];
-   $_POST["email"]) 
+   $_POST["email"];
+  ) 
 {
 mail("imaina671@gmail.com", "Email Notification",
 $_POST["Thanks!"]. "From: jane@janedoe.com");
